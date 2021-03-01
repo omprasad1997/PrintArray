719bb12c1bc551ed6bd1b779f8ab4daa6366f8ba
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-01 18:16:30 +0530

    Added:Create PrintArray class and toPrint method to print corresponding elements

--- main/java/com/printarray/PrintArray.java
+++ main/java/com/printarray/PrintArray.java
@@ -2,7 +2,7 @@ package com.printarray;
 
 public class PrintArray {
     public static void main(String[] args) {
-        int[] a ={1,2,3};
+        int[] a = {1,2,3};
         double[] b = {1.1,2.2,3.3};
         char[] c = {'a','b','c'};
         toPrint(a);
