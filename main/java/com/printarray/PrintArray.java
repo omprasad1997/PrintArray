80f16902a37e5ae23475dfcc139b1e2516bffd30
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-01 19:06:23 +0530

    Updated:Created generics class that will take different data types

--- main/java/com/printarray/PrintArray.java
+++ main/java/com/printarray/PrintArray.java
@@ -1,14 +1,26 @@
 package com.printarray;
 
-public class PrintArray {
+public class PrintArray<E> {
+    E[] myArray;
+
+    public PrintArray(E[] myArray) {
+        this.myArray = myArray;
+    }
+
     public static void main(String[] args) {
         Integer[] a = {1,2,3};
         Double[] b = {1.1,2.2,3.3};
         Character[] c = {'a','b','c'};
-        toPrint(a);
-        toPrint(b);
-        toPrint(c);
+
+        new PrintArray<Integer>(a).toPrint();
+        new PrintArray<Double>(b).toPrint();
+        new PrintArray<Character>(c).toPrint();
     }
+
+    private void toPrint() {
+        toPrint(myArray);
+    }
+
     private static<E> void toPrint(E[] a) {
         for(E i:a)
         {
